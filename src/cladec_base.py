78c33f8848517ed6ba94333ef583375eed680e38
Tom Krueger <76523782+tomtuamnuq@users.noreply.github.com>
2022-07-26 21:33:29 +0200

    use squared distance as reconstruction loss function

--- src/cladec_base.py
+++ src/cladec_base.py
@@ -17,16 +17,19 @@ class ClaDecBase(keras.Model, abc.ABC):
         self.encoder = None
         self.decoder = None
         self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
-        self.reconstruction_loss_tracker = keras.metrics.Mean(
-            name="reconstruction_loss"
-        )
+        self.reconstruction_loss_tracker = keras.metrics.Mean(name="reconstruction_loss")
         self.class_loss_tracker = keras.metrics.Mean(name="class_loss")
-        self.reconstruction_loss_fn = losses.MeanSquaredError()
 
     @tf.function
     def classification_loss_fn(self, x, y):
         y_prime = self.classifier(x)
-        return tf.reduce_mean(self.classifier.loss(y, y_prime))
+        return tf.reduce_mean(self.classifier.compiled_loss(y, y_prime))
+
+    @staticmethod
+    @tf.function
+    def reconstruction_loss_fn(x, y, axis=(1, 2)):
+        """Mean of squared distance over the batch. Default axis for image data."""
+        return tf.reduce_mean(tf.reduce_sum(tf.square(x - y), axis=axis))
 
     @abc.abstractmethod
     def call(self, inputs):
@@ -34,11 +37,7 @@ class ClaDecBase(keras.Model, abc.ABC):
 
     @property
     def metrics(self):
-        return [
-            self.total_loss_tracker,
-            self.reconstruction_loss_tracker,
-            self.class_loss_tracker
-        ]
+        return [self.total_loss_tracker, self.reconstruction_loss_tracker, self.class_loss_tracker]
 
     @abc.abstractmethod
     def create_encoder(self):
@@ -58,9 +57,21 @@ class ClaDecBase(keras.Model, abc.ABC):
     def create_128_dense_decoder():
         latent_inputs = keras.Input(shape=128)
         decoder = layers.Reshape((1, 1, 128))(latent_inputs)
-        decoder = layers.Conv2DTranspose(64, (4, 4), padding='valid', strides=(2, 2), activation='relu')(decoder)
-        decoder = layers.Conv2DTranspose(32, (3, 3), padding='valid', strides=(2, 2), activation='relu')(decoder)
-        decoder = layers.Conv2DTranspose(1, (3, 3), padding='valid', strides=(3, 3), activation='sigmoid',
+        decoder = layers.Conv2DTranspose(64,
+                                         (4, 4),
+                                         padding='valid',
+                                         strides=(2, 2),
+                                         activation='relu')(decoder)
+        decoder = layers.Conv2DTranspose(32,
+                                         (3, 3),
+                                         padding='valid',
+                                         strides=(2, 2),
+                                         activation='relu')(decoder)
+        decoder = layers.Conv2DTranspose(1,
+                                         (3, 3),
+                                         padding='valid',
+                                         strides=(3, 3),
+                                         activation='sigmoid',
                                          output_padding=1)(decoder)
 
         return Model(latent_inputs, decoder, name="decoder")
@@ -69,8 +80,16 @@ class ClaDecBase(keras.Model, abc.ABC):
     def create_64_conv_decoder():
         latent_inputs = keras.Input(shape=3136)
         decoder = layers.Reshape((7, 7, 64))(latent_inputs)
-        decoder = layers.Conv2DTranspose(64, (4, 4), padding='same', strides=(2, 2), activation='relu')(decoder)
-        decoder = layers.Conv2DTranspose(1, (4, 4), padding='same', strides=(2, 2), activation='sigmoid')(decoder)
+        decoder = layers.Conv2DTranspose(64,
+                                         (4, 4),
+                                         padding='same',
+                                         strides=(2, 2),
+                                         activation='relu')(decoder)
+        decoder = layers.Conv2DTranspose(1,
+                                         (4, 4),
+                                         padding='same',
+                                         strides=(2, 2),
+                                         activation='sigmoid')(decoder)
         return Model(latent_inputs, decoder, name="decoder")
 
     @abc.abstractmethod
--- src/create_models.py
+++ src/create_models.py
@@ -8,7 +8,11 @@ from keras.models import Sequential, Model
 import src.utils
 from src.cladec import ClaDec, RefAE
 from src.classifier import create_classifier_model_compiled
-from src.utils import keras_dataset_image_preprocessing, get_optimizer, DENSE_LAYER_NAME, CONV_LAYER_NAME, ALPHAS
+from src.utils import (keras_dataset_image_preprocessing,
+                       get_optimizer,
+                       DENSE_LAYER_NAME,
+                       CONV_LAYER_NAME,
+                       ALPHAS, )
 
 tf.random.set_seed(1234)
 file_path = os.path.dirname(os.path.realpath("__file__"))
@@ -22,7 +26,8 @@ def train_classifier(x, y, epochs):
     return classifier
 
 
-def train_cladec(x, y, classifier: Sequential, layer_name: str, alpha: int, epochs: int, decoder: Model = None):
+def train_cladec(x, y, classifier: Sequential, layer_name: str, alpha: int, epochs: int,
+                 decoder: Model = None):
     claDec = ClaDec(classifier, layer_name, alpha / 100, decoder)
     claDec.compile(optimizer=get_optimizer(learning_rate=0.005))
     claDec.fit(x, y, epochs=epochs)
@@ -45,7 +50,7 @@ def create_models(epochs: int):
         classifier = train_classifier(train_images, train_labels_c, epochs)
         classifier.save(saved_models_path.joinpath('classifier'))
         print("Saved classifier to ", saved_models_path.joinpath('classifier'))
-        for alpha in ALPHAS:  # [0]
+        for alpha in ALPHAS:
             saved_models_path_alpha = saved_models_path.joinpath('cladec').joinpath(f'{alpha:2}')
             saved_models_path_refae = saved_models_path.joinpath('refae')
             for layer_name in layers:  # ['my_dense']
@@ -54,12 +59,19 @@ def create_models(epochs: int):
                     decoder = ClaDec.create_128_dense_decoder()
                 elif layer_name == 'my_conv':
                     decoder = ClaDec.create_64_conv_decoder()
-                cladec = train_cladec(train_images, train_labels_c, classifier, layer_name, alpha, epochs, decoder)
+                cladec = train_cladec(train_images,
+                                      train_labels_c,
+                                      classifier,
+                                      layer_name,
+                                      alpha,
+                                      epochs,
+                                      decoder)
                 cladec.save_weights(saved_models_path_alpha.joinpath(layer_name).joinpath(layer_name))
                 print("Saved cladec to ", saved_models_path_alpha.joinpath(layer_name))
                 if alpha == 0:
                     refae = train_refae(train_images, cladec, epochs)
-                    refae.save_weights(saved_models_path_refae.joinpath(layer_name).joinpath(layer_name))
+                    refae.save_weights(saved_models_path_refae.joinpath(layer_name).joinpath(
+                        layer_name))
                     print("Saved refae to ", saved_models_path_refae.joinpath(layer_name))
 
 
@@ -72,10 +84,14 @@ def test_create_models():
     alpha = 0
     saved_models_path = src.utils.SAVED_MODELS_PATH_MNIST.joinpath('cladec').joinpath(f'{alpha:2}').joinpath(
         DENSE_LAYER_NAME).joinpath(DENSE_LAYER_NAME)
-    saved_models_path_refae = src.utils.SAVED_MODELS_PATH_MNIST.joinpath('refae').joinpath(DENSE_LAYER_NAME).joinpath(
-        DENSE_LAYER_NAME)
+    saved_models_path_refae = src.utils.SAVED_MODELS_PATH_MNIST.joinpath('refae').joinpath(
+        DENSE_LAYER_NAME).joinpath(DENSE_LAYER_NAME)
     decoder = ClaDec.create_128_dense_decoder()
-    cladec = ClaDec.create_from_weights(saved_models_path, classifier, DENSE_LAYER_NAME, alpha / 100, decoder)
+    cladec = ClaDec.create_from_weights(saved_models_path,
+                                        classifier,
+                                        DENSE_LAYER_NAME,
+                                        alpha / 100,
+                                        decoder)
     print("Evaluating cladec:")
     cladec.evaluate(test_images, test_labels_c)
     pred = cladec(test_images)
@@ -87,5 +103,4 @@ def test_create_models():
 
 if __name__ == '__main__':
     sys.stdout = open(src.utils.SAVED_MODELS_BASE_PATH.joinpath("std_output.txt"), "w")
-    create_models(10)
-    # test_create_models()
+    create_models(10)  # 10  # test_create_models()
